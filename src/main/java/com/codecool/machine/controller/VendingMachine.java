2350b6d9bd2560e84ec333bf9b1afba8d826a6cc
Adam Sallai <yadam1998@gmail.com>
2021-02-02 14:25:44 +0100

    add constructor and setter for VendingMachine class

--- src/main/java/com/codecool/machine/controller/VendingMachine.java
+++ src/main/java/com/codecool/machine/controller/VendingMachine.java
@@ -1,9 +1,27 @@
 package com.codecool.machine.controller;
 
+import java.util.Scanner;
+
+import com.codecool.machine.model.Product;
+import com.codecool.machine.model.VendingModel;
+import com.codecool.machine.view.VendingView;
+
 public class VendingMachine {
 	
-	public VendingMachine() {
-		
+	private VendingModel model;
+	private VendingView view;
+	private Product product;
+	private Scanner scanner;
+	
+	
+	public VendingMachine(VendingModel model, VendingView view) {
+		this.model = model;
+		this.view = view;
+		setScanner(new Scanner(System.in));
 	}
 
+
+	public void setScanner(Scanner scanner) {
+		this.scanner = scanner;
+	}
 }
