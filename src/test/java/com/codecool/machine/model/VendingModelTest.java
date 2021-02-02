71df0da064b57ba4c9386c2821debb6b4b817d22
Adam Sallai <yadam1998@gmail.com>
2021-02-02 16:01:35 +0100

    add two more test case to vending model getProductByName method

--- src/test/java/com/codecool/machine/model/VendingModelTest.java
+++ src/test/java/com/codecool/machine/model/VendingModelTest.java
@@ -9,10 +9,30 @@ class VendingModelTest {
 	private VendingModel vendingModel = new VendingModel();
 	
 	@Test
-	public void testGetProductByName() throws Exception {
+	public void testGetProductByNameCoke() throws Exception {
 		Product expectedProduct = Product.COKE;
 
 		Product actualProduct = vendingModel.getProductByName("coke");
+		
+		assertEquals(expectedProduct, actualProduct);
+		
+	}
+	
+	@Test
+	public void testGetProductByNamePepsi() throws Exception {
+		Product expectedProduct = Product.PEPSI;
+
+		Product actualProduct = vendingModel.getProductByName("pepsi");
+		
+		assertEquals(expectedProduct, actualProduct);
+		
+	}
+	
+	@Test
+	public void testGetProductByNameSoda() throws Exception {
+		Product expectedProduct = Product.SODA;
+
+		Product actualProduct = vendingModel.getProductByName("soda");
 		
 		assertEquals(expectedProduct, actualProduct);
 		
