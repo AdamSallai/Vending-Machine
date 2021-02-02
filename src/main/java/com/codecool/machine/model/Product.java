2b3c2aa8e03c6b86e91abdd30819b6449ff8c28b
Adam Sallai <yadam1998@gmail.com>
2021-02-02 14:13:34 +0100

    add test to enum coke cost

--- src/main/java/com/codecool/machine/model/Product.java
+++ src/main/java/com/codecool/machine/model/Product.java
@@ -1,5 +1,17 @@
 package com.codecool.machine.model;
 
 public enum Product {
+	COKE(25);
+
+	
+	private int cost;
+	
+	private Product(int cost) {
+		this.cost = cost;
+	}
+	
+	public int getCost() {
+		return cost;
+	}
 
 }
--- src/test/java/com/codecool/machine/model/ProductTest.java
+++ src/test/java/com/codecool/machine/model/ProductTest.java
@@ -6,4 +6,12 @@ import org.junit.jupiter.api.Test;
 
 class ProductTest {
 
+	@Test
+	public void testCokeProductCost() {
+		Product cokeProduct = Product.COKE;
+		
+		int cost = cokeProduct.getCost();
+		
+		assertEquals(25, cost);
+	}
 }
