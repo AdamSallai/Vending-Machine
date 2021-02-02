f033f658127d94bd5d303e553eff3d745f42da75
Adam Sallai <yadam1998@gmail.com>
2021-02-02 13:48:11 +0100

    add empty test classes

---
+++ src/test/java/com/codecool/machine/controller/VendingMachineTest.java
@@ -0,0 +1,9 @@
+package com.codecool.machine.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VendingMachineTest {
+
+}
---
+++ src/test/java/com/codecool/machine/model/ProductTest.java
@@ -0,0 +1,9 @@
+package com.codecool.machine.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+}
