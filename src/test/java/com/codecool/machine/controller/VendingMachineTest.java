3ff0b5dd848a200ee2d388c575cc9c41bedf9a4e
Adam Sallai <yadam1998@gmail.com>
2021-02-03 11:19:00 +0100

    refactor test case, remove unneccesary imports

--- src/test/java/com/codecool/machine/controller/VendingMachineTest.java
+++ src/test/java/com/codecool/machine/controller/VendingMachineTest.java
@@ -2,16 +2,9 @@ package com.codecool.machine.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.Scanner;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import com.codecool.machine.model.Product;
 import com.codecool.machine.model.VendingModel;
@@ -31,7 +24,6 @@ class VendingMachineTest {
 	@Test
 	public void testSelectProductWithValidInput() {
 	    String input = "coke";
-	    InputStream in = new ByteArrayInputStream(input.getBytes());
 
 	    Scanner scanner = new Scanner(input);
 	    vendingMachine.setScanner(scanner);
